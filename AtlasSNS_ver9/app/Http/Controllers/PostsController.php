0311ab510c6444e04e8f9d71460d321df503d4d3
内田奈津子 <>
2024-12-20 17:30:57 +0900

    ・Authenticade.phpのroute('login')をroute('auth.login)に変更しました。 ・auth.phpにログイン中にアクセス制限をかけて繋がるように、auth.phpでログイン中のルーティングコードを対象画面のみ記述しました。 ・PostsController.phpにuse宣言を追加しました。 ・その下に、コンストラクタメソッドを追加しました。 ・public function indexメソッドの中に、ログインしたユーザー情報を取得できるようにコードを追加しました。 ・一番下にlogoutのメソッドを追加しました。

--- AtlasSNS_ver9/app/Http/Controllers/PostsController.php
+++ AtlasSNS_ver9/app/Http/Controllers/PostsController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Auth認証のuse宣言追加↓
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');//auth認証
+        // PostsController/phpを読み込む前にauth機能を実行
+    }
     //
+
     public function index(){
+
+        $user = Auth::user(); //ログインしたユーザーの情報を取得する
+        $username = Auth::user()->username; //上記で取得したユーザー情報からusernameの情報だけ取得する
         return view('posts.index');
+    }
+
+    public function logout()
+    {
+        return view('auth.login');
     }
 }
--- AtlasSNS_ver9/routes/auth.php
+++ AtlasSNS_ver9/routes/auth.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
+
+// ログアウト中のページ
+
 Route::middleware('guest')->group(function () {
 
     Route::get('/login', [AuthenticatedSessionController::class, 'create']);
@@ -17,4 +20,29 @@ Route::middleware('guest')->group(function () {
     Route::get('/added', [RegisteredUserController::class, 'added']);
     Route::post('/added', [RegisteredUserController::class, 'added']);
 
+});
+
+// ログイン中のページ
+
+Route::group(['middleware' => 'auth'], function () {
+
+    // トップページへ
+    Route::get('/top', [PostsController::class, 'index']);//取得するため
+    Route::post('/top', [PostsController::class, 'index']);//指定のルートへ
+
+    // プロフィール編集ページへ
+    Route::get('/profile', [UsersController::class, 'profile']);
+
+// ユーザー検索ページへ
+    Route::get('/search', [UsersController::class, 'search']);
+
+    Route::post('/follow-list', [PostsController::class, 'followList']);
+    Route::post('/follower-list', [PostsController::class, 'followerList']);
+
+    // ログアウト
+    Route::get('/logout', [PostsController::class, 'logout']);
+
+    // フォロー、フォロワーページへ
+    Route::get('/follow-list', [FollowsController::class, 'followList']);
+    Route::get('/follower-list', [FollowsController::class, 'followerList']);
 });
