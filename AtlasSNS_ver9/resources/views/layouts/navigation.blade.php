51da946dfc911aeb36a830fce5f2f66d4e9fbd42
内田奈津子 <>
2024-12-20 17:34:01 +0900

    アトラスロゴにヘッダーへ戻るリンクを設定しました

--- AtlasSNS_ver9/resources/views/layouts/navigation.blade.php
+++ AtlasSNS_ver9/resources/views/layouts/navigation.blade.php
@@ -1,8 +1,9 @@
         <div id="head">
-            <!-- ↓アトラスロゴにヘッダーへ戻るリンクを設定 -->
+            <!-- ↓↓アトラスロゴにヘッダーへ戻るリンクを設定 -->
             <h1><a href="/top"><img src="images/atlas.png" alt="Atlas"></a></h1>
             <div id="">
                 <div id="">
+                    <!-- ログイン名を表示させる↓ -->
                     <p>{{Auth::user()->username}}さん<img src="images/icon1.png"></p>
                 </div>
                 <ul>
