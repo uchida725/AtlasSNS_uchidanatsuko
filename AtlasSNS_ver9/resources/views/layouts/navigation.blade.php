d8c08f3abad78e137fba5eebc14e7b22f4cb67b6
内田奈津子 <>
2025-01-13 12:07:25 +0900

    アコーディオンメニューのコードを入力しました

--- AtlasSNS_ver9/resources/views/layouts/navigation.blade.php
+++ AtlasSNS_ver9/resources/views/layouts/navigation.blade.php
@@ -1,15 +1,21 @@
-        <div id="head">
+ <div id="head">
             <!-- ↓↓アトラスロゴにヘッダーへ戻るリンクを設定 -->
             <h1><a href="/top"><img src="images/atlas.png" alt="Atlas"></a></h1>
-            <div id="">
-                <div id="">
-                    <!-- ログイン名を表示させる↓ -->
-                    <p>{{Auth::user()->username}}さん<img src="images/icon1.png"></p>
-                </div>
-                <ul>
-                    <li><a href="">ホーム</a></li>
-                    <li><a href="">プロフィール</a></li>
-                    <li><a href="">ログアウト</a></li>
+            <!-- <div id="">
+                <div id=""> -->
+                <!-- </div> -->
+                <div class="nav-open">
+                        <p>{{Auth::user()->username}}さん</p>
+                        <nav>
+                            <a href="" class="menu-btn"></a>
+                <ul class="tag">
+                    <li><a href="/top">HOME</a></li>
+                    <li><a href="/profile">プロフィール編集</a></li>
+                    <li><a href="/logout">ログアウト</a></li>
                 </ul>
+    </nav>
+    <div class="icon"><img src="images/icon1.png"></div>
+                    </div>
+                </div>
             </div>
         </div>
