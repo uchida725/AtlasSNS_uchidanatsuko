666f9fce6b0b3e407a9574725c4f2857b85a6c54
内田奈津子 <>
2025-01-13 16:19:09 +0900

    slideDownからslideToggleに変更し、  content.slideToggle();を削除したところ、 アコーディオンメニューを実装できました。

--- AtlasSNS_ver9/public/js/script.js
+++ AtlasSNS_ver9/public/js/script.js
@@ -6,10 +6,9 @@
 $(document).ready(function () {
   $('.menu-btn').on('click', function () {
     const content = $(this).next('.tag');
-    content.slideToggle();
     // 三角ボタンの状態を切り替え
     $(this).toggleClass('open');
-    $('.tag').not(content).slideDown();
+    $('.tag').not(content).slideToggle();
 
   });
 });
