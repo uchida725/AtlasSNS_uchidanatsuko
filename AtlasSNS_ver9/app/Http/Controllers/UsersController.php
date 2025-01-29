317c3a158a368a560eef6d3214b3f6fa58377b63
内田奈津子 <>
2025-01-29 15:14:33 +0900

    プロフィール編集画面のコントローラーを作成しました

--- AtlasSNS_ver9/app/Http/Controllers/UsersController.php
+++ AtlasSNS_ver9/app/Http/Controllers/UsersController.php
@@ -6,6 +6,26 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+//    プロフィール編集機能
+public function updateProfile(Request $request)
+{
+    $id = $request->inpput('id');
+    $username = $request->input('username');
+    $mail = $request->input('mail');
+    $password = $request->input('password');
+    $bio = $request->input('bio');
+
+    User::where('id', $id)->update([
+        'username' => $username,
+        'mail' => $mail,
+        'password'=> Hash::make($request->password),
+        'bio' => $bio,
+    ]);
+
+    return redirect('/top');
+}
+
+
     //
     public function search(){
         return view('users.search');
